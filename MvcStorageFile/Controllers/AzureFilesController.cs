750ece24a05a4059f9faa69694fd9726a6d61a52
serraguti <pacoserranox@gmail.com>
2021-03-09 10:56:39 +0100

    commit

---
+++ MvcStorageFile/Controllers/AzureFilesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MvcStorageFile.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStorageFile.Controllers
+{
+    public class AzureFilesController : Controller
+    {
+        ServiceStorageFile servicefiles;
+
+        public AzureFilesController(ServiceStorageFile servicefiles)
+        {
+            this.servicefiles = servicefiles;
+        }
+
+        public async Task<IActionResult> Index(String filename)
+        {
+            if (filename != null)
+            {
+                String content =
+                    await this.servicefiles.GetFileContentAsync(filename);
+                ViewData["CONTENT"] = content;
+            }
+            List<String> files =await this.servicefiles.GetFilesAsync();
+            return View(files);
+        }
+
+        public IActionResult UploadFile()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile(IFormFile file)
+        {
+            String filename = file.FileName;
+            using (var stream = file.OpenReadStream())
+            {
+                await this.servicefiles.UploadFileAsync(filename, stream);
+            }
+            ViewData["MENSAJE"] = "Archivo subido correctamente";
+            return View();
+        }
+
+        public async Task<IActionResult> DeleteFile(String filename)
+        {
+            await this.servicefiles.DeleteFileAsync(filename);
+            return RedirectToAction("Index");
+        }
+    }
+}
--- MvcStorageFile/Services/ServiceStorageFile.cs
+++ MvcStorageFile/Services/ServiceStorageFile.cs
@@ -1,20 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Azure;
 using Azure.Storage.Files.Shares;
+using Azure.Storage.Files.Shares.Models;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
 
 namespace MvcStorageFile.Services
 {
     public class ServiceStorageFile
     {
-        private ShareClient client;
+        private ShareDirectoryClient root;
 
         public ServiceStorageFile()
         {
             String keys = "DefaultEndpointsProtocol=https;AccountName=storagetajamarpgs;AccountKey=AZN0grugwrgClnhivSv5up4VD2OAwT2m8Hk4j1z3BZ+MAYUAIUBc3T6JaQHL1vDSP6zKJikQm3bFB+gob1gMMQ==;EndpointSuffix=core.windows.net";
-            this.client =
+            ShareClient client = 
                 new ShareClient(keys, "ejemplo");
+            this.root = client.GetRootDirectoryClient();
+        }
+
+        public async Task<List<String>> GetFilesAsync()
+        {
+            List<String> files = new List<string>();
+            await foreach (var file in this.root.GetFilesAndDirectoriesAsync())
+            {
+                files.Add(file.Name);
+            }
+            return files;
+        }
+
+        public async Task<String> GetFileContentAsync(String filename)
+        {
+            ShareFileClient file = this.root.GetFileClient(filename);
+            var data = await file.DownloadAsync();
+            Stream stream = data.Value.Content;
+            StreamReader reader = new StreamReader(stream);
+            return await reader.ReadToEndAsync();
+        }
+
+        public async Task DeleteFileAsync(String filename)
+        {
+            ShareFileClient file = this.root.GetFileClient(filename);
+            await file.DeleteAsync();
+        }
+
+        public async Task UploadFileAsync(String filename
+            , Stream stream)
+        {
+            ShareFileClient file = this.root.GetFileClient(filename);
+            await file.CreateAsync(stream.Length);
+            await file.UploadAsync(stream);
         }
     }
 }
