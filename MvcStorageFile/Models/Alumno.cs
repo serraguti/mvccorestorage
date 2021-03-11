2e53c08602ef888bd829f57d70597aa1f89fc9c6
serraguti <pacoserranox@gmail.com>
2021-03-11 10:55:19 +0100

    commit

---
+++ MvcStorageFile/Models/Alumno.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStorage.Models
+{
+    public class Alumno
+    {
+        public String IdAlumno { get; set; }
+        public String Nombre { get; set; }
+        public String Apellidos { get; set; }
+        public String Curso { get; set; }
+        public String Nota { get; set; }
+    }
+}
