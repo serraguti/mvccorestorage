133f4f2dbde41a0b9cb12dc79b07e2e74dadca67
serraguti <pacoserranox@gmail.com>
2021-03-11 11:18:18 +0100

    com

---
+++ MvcStorageFile/Controllers/AlumnosController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStorage.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStorage.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        ServiceTableAlumnos ServiceAlumnos;
+
+        public AlumnosController(ServiceTableAlumnos servicealumnos)
+        {
+            this.ServiceAlumnos = servicealumnos;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(String curso)
+        {
+            String token = await this.ServiceAlumnos.GetTokenAsync(curso);
+            return View(this.ServiceAlumnos.GetAlumnos(token));
+        }
+    }
+}
---
+++ MvcStorageFile/Services/ServiceTableAlumnos.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using MvcStorage.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MvcStorage.Services
+{
+    public class ServiceTableAlumnos
+    {
+        //METODO ASYNC PARA PODER CONSUMIR EL TOKEN
+        public async Task<String> GetTokenAsync(String curso)
+        {
+            String request = "api/access/token/" + curso;
+            String url = "https://localhost:44342/";
+            using (WebClient client = new WebClient())
+            {
+                client.Headers["content-type"] = "application/json";
+                String content =
+                    await client.DownloadStringTaskAsync(new Uri(url + request));
+                JObject jobject = JObject.Parse(content);
+                return jobject.GetValue("token").ToString();
+            }
+        }
+
+        public List<Alumno> GetAlumnos(String token)
+        {
+            String uristorage = "http://127.0.0.1:10002/devstoreaccount1/";
+            StorageCredentials credentials =
+                new StorageCredentials(token);
+            CloudTableClient client =
+                new CloudTableClient(new Uri(uristorage), credentials);
+            CloudTable tabla = client.GetTableReference("tablaalumnos");
+            TableQuery<Alumno> query = new TableQuery<Alumno>();
+            return tabla.ExecuteQuery(query).ToList();
+        }
+    }
+}
